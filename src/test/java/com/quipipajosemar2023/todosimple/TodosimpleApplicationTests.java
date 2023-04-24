8c90f9c24f3bf51e237f29484efa53be80b83e14
Josemar Quipipa <josemarquipipa1994@hotamil.com>
2023-04-24 19:18:41 +0100

    Spring Boot Project

---
+++ src/test/java/com/quipipajosemar2023/todosimple/TodosimpleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quipipajosemar2023.todosimple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodosimpleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
